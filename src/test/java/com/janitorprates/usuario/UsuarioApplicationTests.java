672339bbf3f751f1072de73d2c4acb0995df8090
Janitor Prates <janitorprates@gmail.com>
2026-09-06 06:45:04 -0300

    commit inicial

---
+++ src/test/java/com/janitorprates/usuario/UsuarioApplicationTests.java
@@ -0,0 +1,13 @@
+package com.janitorprates.usuario;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsuarioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
