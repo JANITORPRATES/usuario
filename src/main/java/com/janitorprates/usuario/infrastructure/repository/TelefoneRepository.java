6e2ca6eb5e935833c77d664af0d6925112714b04
Janitor Prates <janitorprates@gmail.com>
2026-09-06 07:39:25 -0300

    adicionadas classes de security

---
+++ src/main/java/com/janitorprates/usuario/infrastructure/repository/TelefoneRepository.java
@@ -0,0 +1,9 @@
+package com.janitorprates.usuario.infrastructure.repository;
+
+import com.janitorprates.usuario.infrastructure.entity.Telefone;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TelefoneRepository extends JpaRepository<Telefone,Long> {
+}
